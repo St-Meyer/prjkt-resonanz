34f2dd19409193acd626153897fda478f325b1d8
Steffen Meyer <Meyer.92@gmx.de>
2026-06-07 20:45:58 +0200

    Add coyote time to player movement

--- PrkjtResonanz/scripts/player/Lyra.cs
+++ PrkjtResonanz/scripts/player/Lyra.cs
@@ -2,23 +2,31 @@ using Godot;
 using System;
 
 public partial class Lyra : CharacterBody2D
-{
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
+{	[Export]
+	public float Speed = 300.0f;
+	[Export]
+	public float JumpVelocity = -400.0f;
+	[Export]
+	public float CoyoteTime = 0.1f;
+	private float _coyoteTimer = 0f;
 
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
-
-
-		// Add the gravity.
+		// Coyote Timer
+		if (IsOnFloor()){
+			_coyoteTimer = CoyoteTime;
+		}
 		if (!IsOnFloor())
 		{
+			if (_coyoteTimer > 0) {
+				_coyoteTimer -= (float)delta;
+			}
+			// Add the gravity.
 			velocity += GetGravity() * (float)delta;
 		}
-
 		// Handle Jump.
-		if (Input.IsActionJustPressed("jump") && IsOnFloor())
+		if (Input.IsActionJustPressed("jump") && (IsOnFloor() || _coyoteTimer > 0))
 		{
 			velocity.Y = JumpVelocity;
 		}
@@ -33,7 +41,6 @@ public partial class Lyra : CharacterBody2D
 		{
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
 		}
-
 		Velocity = velocity;
 		MoveAndSlide();
 	}
