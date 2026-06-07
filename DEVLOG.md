b69583d157b0af6b0d58a63fda16306c7e4fa5a8
Steffen Meyer <Meyer.92@gmx.de>
2026-06-07 15:14:58 +0200

    Add Lyra Player Scene with basic Movement and jump

---
+++ DEVLOG.md
@@ -0,0 +1,1 @@
+2026-06-07: Ordnerstruktur aufgebaut, Initialen Git-Commit, erstes Movement des Player-Charakters.
---
+++ PrkjtResonanz/scripts/player/Lyra.cs
@@ -0,0 +1,40 @@
+using Godot;
+using System;
+
+public partial class Lyra : CharacterBody2D
+{
+	public const float Speed = 300.0f;
+	public const float JumpVelocity = -400.0f;
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Velocity;
+
+
+		// Add the gravity.
+		if (!IsOnFloor())
+		{
+			velocity += GetGravity() * (float)delta;
+		}
+
+		// Handle Jump.
+		if (Input.IsActionJustPressed("jump") && IsOnFloor())
+		{
+			velocity.Y = JumpVelocity;
+		}
+
+		// Get the input direction and handle the movement/deceleration.
+		float direction = Input.GetAxis("move_left", "move_right");
+		if (direction != 0)
+		{
+			velocity.X = direction * Speed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
