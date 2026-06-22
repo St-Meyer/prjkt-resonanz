36b3262347166c4055c829f66754fb77f9ed214a
Steffen Meyer <Meyer.92@gmx.de>
2026-06-22 16:45:33 +0200

    initial refactoring of player script to oop methods

--- PrkjtResonanz/scripts/enemies/TestEnemy.cs
+++ PrkjtResonanz/scripts/enemies/TestEnemy.cs
@@ -15,32 +15,24 @@ public partial class TestEnemy : CharacterBody2D
 	private Area2D _attackHitbox;
 	private AnimatedSprite2D _animatedSprite2D;
 	
-
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready(){
-		GD.Print("READY CALLED");
 		_currentHealth = MaxHealth;
-		GD.Print("HEALTH GESETZT");
-	
+		
 		// Animation Logic
 		_animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-		GD.Print("ANIMATED SPRITE GEFUNDEN");
 		_animatedSprite2D.AnimationFinished += OnAnimationFinished;
 		_animatedSprite2D.Play("idle");
-		GD.Print("ANIMATION GESTARTET");
 		
 		// Referenz zu AttackHitBox
 		_attackHitbox = GetNode<Area2D>("AttackHitbox");
-		GD.Print("TestEnemy geladen. HitboxMonitoring: " + _attackHitbox.Monitoring);
 		_attackHitbox.Monitoring = true;
 		
 		// abonieren von AttackHitBox
 		_attackHitbox.BodyEntered += OnAttackHitboxBodyEntered;
-		GD.Print("ALLES BEREIT");
 	}
 
 	public void OnAttackHitboxBodyEntered(Node2D body) {
-		GD.Print("Hitbox getroffen von: " + body.Name);
 		if (body is Player player) {
 			player.GetHit(Strength);
 		}
@@ -53,7 +45,6 @@ public partial class TestEnemy : CharacterBody2D
 	public void TakeDamage(int damage){
 		if (_currentHealth > 0) {
 			_currentHealth -= damage;
-			GD.Print(_currentHealth);
 			if (_currentHealth <= 0) {
 				_animatedSprite2D.Play("death");
 			}
