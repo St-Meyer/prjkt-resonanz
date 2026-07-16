d55cc0a09657f4d092deda231a18798f3d040241
Steffen Meyer <Meyer.92@gmx.de>
2026-07-16 11:19:10 +0200

    Method for loading safe data from json and method for checking, if save data exists.

--- PrjktResonanz/scripts/core/SaveManager.cs
+++ PrjktResonanz/scripts/core/SaveManager.cs
@@ -16,16 +16,23 @@ public partial class SaveManager : Node
         FileAccess json = FileAccess.Open("user://save_" + slotID + ".json", FileAccess.ModeFlags.Write);
         string jsonString = JsonSerializer.Serialize(_save);
         json.StoreString(jsonString);
-
     }
 
-    public SaveData Load()
+    public SaveData Load(int slotID)
     {
-        return null;
+        SaveData _save = new SaveData();
+        string jsonPath = "user://save_" + slotID + ".json";
+        var json = FileAccess.Open(jsonPath, FileAccess.ModeFlags.Read);
+        if (json != null)
+        {
+            _save = JsonSerializer.Deserialize<SaveData>(json.GetAsText());
+        }
+
+        return _save;
     }
 
     public bool HasSave(int slotID)
     {
-        return false;
+        return FileAccess.FileExists("user://save_" + slotID + ".json");
     }
 }
