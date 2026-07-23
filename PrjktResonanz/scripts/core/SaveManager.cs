eb1f1d4e087c64c658b7d7327b85372f071b3516
Steffen Meyer <Meyer.92@gmx.de>
2026-07-23 09:56:04 +0200

    corrected parameter names in SaveManager.

--- PrjktResonanz/scripts/core/SaveManager.cs
+++ PrjktResonanz/scripts/core/SaveManager.cs
@@ -4,35 +4,35 @@ using FileAccess = Godot.FileAccess;
 
 public partial class SaveManager : Node
 {
-    public void Save(int savepointID, int slotID)
+    public void Save(int savepointId, int slotId)
     {
         PlayerData playerData = GetNode<PlayerData>("/root/PlayerData");
-        SaveData _save = new SaveData()
+        SaveData save = new SaveData()
         {
-            SlotID = slotID,
-            SavePointID = savepointID,
+            SlotID = slotId,
+            SavePointID = savepointId,
             CurrentHealth = playerData.CurrentHealth
         };
-        FileAccess json = FileAccess.Open("user://save_" + slotID + ".json", FileAccess.ModeFlags.Write);
-        string jsonString = JsonSerializer.Serialize(_save);
+        FileAccess json = FileAccess.Open("user://save_" + slotId + ".json", FileAccess.ModeFlags.Write);
+        string jsonString = JsonSerializer.Serialize(save);
         json.StoreString(jsonString);
     }
 
-    public SaveData Load(int slotID)
+    public SaveData Load(int slotId)
     {
-        SaveData _save = new SaveData();
-        string jsonPath = "user://save_" + slotID + ".json";
+        SaveData save = new SaveData();
+        string jsonPath = "user://save_" + slotId + ".json";
         var json = FileAccess.Open(jsonPath, FileAccess.ModeFlags.Read);
         if (json != null)
         {
-            _save = JsonSerializer.Deserialize<SaveData>(json.GetAsText());
+            save = JsonSerializer.Deserialize<SaveData>(json.GetAsText());
         }
 
-        return _save;
+        return save;
     }
 
-    public bool HasSave(int slotID)
+    public bool HasSave(int slotId)
     {
-        return FileAccess.FileExists("user://save_" + slotID + ".json");
+        return FileAccess.FileExists("user://save_" + slotId + ".json");
     }
 }
