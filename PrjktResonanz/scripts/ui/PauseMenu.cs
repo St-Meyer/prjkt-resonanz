0bb89430feca23ab1aadebe0e233d1a989eb92cc
Steffen Meyer <Meyer.92@gmx.de>
2026-08-03 10:52:53 +0200

    Buttons of Tilte Menu linked to logic

--- PrjktResonanz/scripts/ui/PauseMenu.cs
+++ PrjktResonanz/scripts/ui/PauseMenu.cs
@@ -5,4 +5,11 @@ public partial class PauseMenu : Node
     private Button _btnResume;
     private Button _btnLoad;
     private Button _btnTitleMenu;
+
+    public override void _Ready()
+    {
+        _btnResume = GetNode<Button>("HBoxContainer/btnResume");
+        _btnLoad = GetNode<Button>("HBoxContainer/btnLoad");
+        _btnTitleMenu = GetNode<Button>("HBoxContainer/btnTitleMenu");
+    }
 }
