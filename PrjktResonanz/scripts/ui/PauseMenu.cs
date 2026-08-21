359c12fb2b6afb198fd8168098e2677fa6c0b8a3
Steffen Meyer <Meyer.92@gmx.de>
2026-08-21 15:43:44 +0200

    Comments for PauseMenu.cs

--- PrjktResonanz/scripts/ui/PauseMenu.cs
+++ PrjktResonanz/scripts/ui/PauseMenu.cs
@@ -12,27 +12,34 @@ public partial class PauseMenu : Control
         _btnLoad = GetNode<Button>("HBoxContainer/btnLoad");
         _btnTitleMenu = GetNode<Button>("HBoxContainer/btnTitleMenu");
 
+        // Szene wird beim Start unsichtbar gestellt
         this.Visible = false;
 
         _btnResume.Pressed += HandleResumePressed;
         _btnLoad.Pressed += HandleLoadPressed;
         _btnTitleMenu.Pressed += HandleTitleMenuPressed;
 
+        // Szene funktionabel, auch wenn Game pausiert ist
         ProcessMode = ProcessModeEnum.Always;
     }
 
+    // Game wird pausiert und Szene wird sichtbar gestellt
     public void ShowPauseMenu()
     {
         GetTree().Paused = true;
         this.Visible = true;
     }    
 
+    // Game wird fortgeführt und Szene wird unsichtbar gestellt
     private void HandleResumePressed()
     {
         GetTree().Paused = false;
         this.Visible = false;
     }
 
+    // Pause wird beendet und wenn Save File besteht, wird Test-Level geladen
+    // TODO: Auswahl aus 3 Savefiles
+    // TODO: Wechsel auf Szene mit aktuellen Savepoint
     private void HandleLoadPressed()
     {
         GetTree().Paused = false;
@@ -44,6 +51,7 @@ public partial class PauseMenu : Control
         }
     }
 
+    // Pause wird beendet und wechsel auf Title Menu
     private void HandleTitleMenuPressed()
     {
         GetTree().Paused = false;
